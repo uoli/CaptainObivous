d6159efcba7e4092b83de6bb0dad839f0705bc4a
Elvis Alistar <ealis2001@gmail.com>
2015-02-07 00:48:08 +0100

    Change cursor on hover over interactable object

---
+++ CaptainObvious/Assets/Scripts/Cursor.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cursor : MonoBehaviour {
+	public Texture2D normalCursor;
+	public Texture2D interactCursor;
+
+	// Update is called once per frame
+	void Update () {
+		var ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+		RaycastHit hit = new RaycastHit();
+		if (Physics.Raycast (ray, out hit, 1000)) {
+			if (hit.collider.gameObject.tag.Equals("Interactable"))
+			    UnityEngine.Cursor.SetCursor(interactCursor, Vector3.zero, CursorMode.Auto);
+			else
+				UnityEngine.Cursor.SetCursor(normalCursor, Vector3.zero, CursorMode.Auto);
+		}
+	}
+}
