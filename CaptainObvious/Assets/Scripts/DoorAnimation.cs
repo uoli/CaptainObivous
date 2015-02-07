7fecd4a5dbd0a7342d704385a463511d265e0063
Alexey Zakharov <alexeyz@unity3d.com>
2015-02-07 21:07:03 +0100

    Fix door reset while looping

--- CaptainObvious/Assets/Scripts/DoorAnimation.cs
+++ CaptainObvious/Assets/Scripts/DoorAnimation.cs
@@ -1,13 +1,29 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class DoorAnimation : MonoBehaviour {
-
+public class DoorAnimation : MonoBehaviour
+{
 	float time = 0;
+
+	void OnEnable()
+	{
+		time = 0;
+	}
+
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+	{
 		time += Time.deltaTime;
 		float angle = Mathf.LerpAngle(0, -90, time);
 		transform.localEulerAngles = new Vector3(0, angle, 0);
+		if (time >= 1.0f)
+		{
+			enabled = false;
+		}
+	}
+
+	public void Reset()
+	{
+		transform.localEulerAngles = new Vector3(0, 0, 0);
 	}
 }
