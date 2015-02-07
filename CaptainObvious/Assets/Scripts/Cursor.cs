d65d8f920d0d1258706508ad06e85fbade8c31bf
Elvis Alistar <ealis2001@gmail.com>
2015-02-07 01:05:47 +0100

    Extremely basic gameplay

--- CaptainObvious/Assets/Scripts/Cursor.cs
+++ CaptainObvious/Assets/Scripts/Cursor.cs
@@ -4,6 +4,8 @@ using System.Collections;
 public class Cursor : MonoBehaviour {
 	public Texture2D normalCursor;
 	public Texture2D interactCursor;
+	private bool overInteractable = false;
+	private GameObject selectedInteractable;
 
 	// Update is called once per frame
 	void Update () {
@@ -11,9 +13,28 @@ public class Cursor : MonoBehaviour {
 		RaycastHit hit = new RaycastHit();
 		if (Physics.Raycast (ray, out hit, 1000)) {
 			if (hit.collider.gameObject.tag.Equals("Interactable"))
+			{
 			    UnityEngine.Cursor.SetCursor(interactCursor, Vector3.zero, CursorMode.Auto);
+				overInteractable = true;
+				selectedInteractable = hit.collider.gameObject;
+
+			}
 			else
+			{
 				UnityEngine.Cursor.SetCursor(normalCursor, Vector3.zero, CursorMode.Auto);
+				overInteractable = false;
+			}
+		}
+		HandleMouseClick();
+	}
+
+	void HandleMouseClick() {
+		if (Input.GetMouseButtonDown(0) && overInteractable)
+		{
+			if (selectedInteractable.name.Equals("DoorKnob"))
+			    Debug.Log("You lose, Captain Obvious!");
+			if (selectedInteractable.name.Equals("WindowKnob"))
+				Debug.Log("So, you're not such an Average Joe after all!");
 		}
 	}
 }
