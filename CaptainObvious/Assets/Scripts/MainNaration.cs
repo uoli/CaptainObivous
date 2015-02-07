ca8e4d0c0338a83932b198d762da41b9f9b010b7
Elvis Alistar <ealis2001@gmail.com>
2015-02-07 18:41:49 +0100

    First audio test

---
+++ CaptainObvious/Assets/Scripts/MainNaration.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainNaration : MonoBehaviour {
+	public AudioClip[] narationLines;
+	private int loopCount = 0;
+
+	void OnTriggerEnter (Collider col)
+	{
+		if (loopCount < narationLines.Length)
+		{
+			audio.PlayOneShot(narationLines[loopCount]);
+			loopCount++;
+			this.enabled = false;
+		}
+	}
+}
