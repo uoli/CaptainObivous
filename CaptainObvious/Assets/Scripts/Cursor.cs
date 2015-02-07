9e0dbae6748f0d167f3ac48f703bb10b27b030e5
Elvis Alistar <ealis2001@gmail.com>
2015-02-07 16:31:18 +0100

    Picking and throwing objects

--- CaptainObvious/Assets/Scripts/Cursor.cs
+++ CaptainObvious/Assets/Scripts/Cursor.cs
@@ -4,8 +4,11 @@ using System.Collections;
 public class Cursor : MonoBehaviour {
 	public Texture2D normalCursor;
 	public Texture2D interactCursor;
+	private float distOfObjectInFrontOfPlayer = 2f;
+	private float throwForce = 100f;
 	private bool overInteractable = false;
 	private GameObject selectedInteractable;
+	private bool holdsObject = false;
 
 	// Update is called once per frame
 	void Update () {
@@ -29,16 +32,34 @@ public class Cursor : MonoBehaviour {
 	}
 
 	void HandleMouseClick() {
-		if (Input.GetMouseButtonDown(0) && overInteractable)
+		if (Input.GetMouseButtonDown(0))
 		{
-			if (selectedInteractable.name.Equals("DoorKnob"))
+			//throw object
+			if (holdsObject)
+			{
+				selectedInteractable.transform.parent = null;
+				selectedInteractable.rigidbody.isKinematic = false;
+				selectedInteractable.rigidbody.AddForce(transform.forward * throwForce);
+				holdsObject = false;
+			}
+			//pick object
+			else if (overInteractable)
+			{
+				selectedInteractable.rigidbody.isKinematic = true;
+				selectedInteractable.transform.parent = Camera.main.transform;
+				selectedInteractable.transform.position = transform.position + transform.forward * distOfObjectInFrontOfPlayer;
+				holdsObject = true;
+			}
+			
+			/*if (selectedInteractable.name.Equals("DoorKnob"))
 			{
 			    Debug.Log("You lose, Captain Obvious!");
 				GameObject.FindGameObjectWithTag("Door").GetComponent<Animator>().enabled = true;
 				Application.LoadLevelAdditiveAsync(1);
 			}
 			if (selectedInteractable.name.Equals("WindowKnob"))
-				Debug.Log("So, you're not such an Average Joe after all!");
+				Debug.Log("So, you're not such an Average Joe after all!");*/
+
 		}
 	}
 }
