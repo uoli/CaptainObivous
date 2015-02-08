2c4d3ac986c662f6fcdb2eb296006dca8d4896b1
Rene Damm <rene.damm@gmx.net>
2015-02-08 10:07:46 +0100

    Don't allow pressing phone buttons while audio is playing.

--- CaptainObvious/Assets/Scripts/PhoneButton.cs
+++ CaptainObvious/Assets/Scripts/PhoneButton.cs
@@ -7,6 +7,9 @@ public class PhoneButton : MonoBehaviour
 
 	public void OnMouseDown()
 	{
+		if (Camera.main.audio.isPlaying)
+			return;
+			
 		var phone = GameObject.Find("Phone");
 		var phoneComponent = phone.GetComponent<PhoneNew>();
 		phoneComponent.DialDigit((char) ('0' + digit));
